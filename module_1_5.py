1b83b67242d0ca521b6f6d165e0c2a57fd5e406e
MashaKasha-420 <mashakasha.0999@gmail.com>
2024-09-04 19:50:36 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = ('t', 2, True,)
+print('immutable_var', immutable_var)
+# immutable_var [1] = 3 Программа ввыдает ошибку, так как кортеж это неизменяемая  упорядоченная коллекция
+mutable_list = [11, 'd', False]
+mutable_list [2] = True
+print('mutable_list', mutable_list)
+
