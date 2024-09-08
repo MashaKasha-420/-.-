e9de6667d64f906791d37ef869730df5df23797d
MashaKasha-420 <mashakasha.0999@gmail.com>
2024-09-08 19:53:34 +0300

    Add files via upload

---
+++ module_1_dop.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [4, 5, 5, 2]] #содержит списки оценок для каждого ученика в алфавитном порядке.
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'} #содержит неупорядоченную последовательность имён всех учеников в классе.
+Aaron = (5+3+3+5+4)/5
+Bilbo  = (2+2+2+3)/4
+Johnny =(4+5+5+2)/4
+Khendrik = (4+4+3)/3
+Steve  = (4+5+5+2)/4
+GPA_  = {'Aaron': Aaron,  'Bilbo':Bilbo,  'Johnny':Johnny, 'Khendrik':Khendrik, 'Steve':Steve }
+print(GPA_)
