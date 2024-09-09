7dc1409b5f9adf67b3f685185b3a2754c86b55c7
MashaKasha-420 <mashakasha.0999@gmail.com>
2024-09-09 07:48:08 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+first  = 45
+second = 82
+third  = 45
+
+if first==second and second==third and third==first:
+    print(3)
+
+elif first==second or second==third or third==first:
+    print(2)
+
+else:
+    print(0)
