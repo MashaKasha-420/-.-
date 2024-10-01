cba641030440661108680d82e993a248271a0871
MashaKasha-420 <mashakasha.0999@gmail.com>
2024-10-01 07:25:30 +0300

    Add files via upload

--- module_1_dop.py
+++ module_1_dop.py
@@ -1,9 +1,11 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [4, 5, 5, 2]] #содержит списки оценок для каждого ученика в алфавитном порядке.
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'} #содержит неупорядоченную последовательность имён всех учеников в классе.
-Aaron = (5+3+3+5+4)/5
-Bilbo  = (2+2+2+3)/4
-Johnny =(4+5+5+2)/4
-Khendrik = (4+4+3)/3
-Steve  = (4+5+5+2)/4
-GPA_  = {'Aaron': Aaron,  'Bilbo':Bilbo,  'Johnny':Johnny, 'Khendrik':Khendrik, 'Steve':Steve }
-print(GPA_)
+
+students_sort = sorted(students)
+grades_m = []
+for i in grades:
+     s = sum(i)/ len(i)
+     grades_m.append(s)
+
+dict1 = dict(zip(students_sort, grades_m))
+print(dict1)
