a7bbb9542a869cb03e301da5d1982d5a18ccf44a
Matveeva Maria <mashakasha.0999@gmail.com>
2024-10-21 19:10:35 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,21 +1,25 @@
-def send_email (message, recipient, sender = university.help@gmail.com ):
-    symbols = ["@", ".com", ".ru", ".net"]
-    if recipient in symbols:
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif:
-        recipient == sender
+def send_email (message, recipient, sender = 'university.help@gmail.com' ):
+    if "@" not in recipient or "@" not in sender \
+            or not recipient.endswith((".com", ".ru", ".net")) or not sender.endswith((".com", ".ru", ".net")):
+            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+
+    elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
-    elif:
-        sender ==  university.help@gmail.com
+
+    elif sender ==  'university.help@gmail.com':
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        print(message)
+
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+        print(message)
 
 
-send_email("Домашнее задание проверено" , "sobolev_sp@.mail.ru")
+send_email("Домашнее задание проверено" , "sobolev_sp.nail.vu")
+print('-----------------------------')
 
+send_email("Это сообщение для проверки связи", recipient = 'university.help@gmail.com')
+print('-----------------------------')
 
-send_email("Это сообщение для проверки связи", recipient = university.help@gmail.com)
-
-
-send_email("Добрый день! Мы давно не получали от вас домашних  заданий", "mashakasha.0999@gmail.com",  sender = urban.teacher@mail.ru)
+send_email("Добрый день! Мы давно не получали от вас домашних заданий", "mashakasha.0999@gmail.com",  sender = 'urban.teacher@mail.ru')
+print('-----------------------------')
