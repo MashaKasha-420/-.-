b5b507668fd08de96a897c80f2dd5be01ff2bf4d
Matveeva Maria <mashakasha.0999@gmail.com>
2024-12-11 14:10:34 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    inner_function()
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+
+
+#inner_function() # выдает ошибку:"name 'inner_function' is not defined. Did you mean: 'test_function'?"
